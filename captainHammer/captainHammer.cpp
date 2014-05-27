680708a387bbcfbc8935ef96a5c83d7e69761fdc
teddy-hoo <hulingchuan@hotmail.com>
2014-05-27 20:42:36 +0800

    add code

--- captainHammer/captainHammer.cpp
+++ captainHammer/captainHammer.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
 #include<fstream>
+#include<iomanip>
 #include<math.h>
+#include<stdio.h>
+
+#define PI 3.14159265
+#define G 9.8
 
 using namespace std;
 
 void main(){
-	
+	ifstream dataFile("B-small-practice.in");
+	ofstream resultFile("B-small-practice.out");
+	double velocity;
+	double distance;
+	double theta;
+	int caseCount;
+	dataFile >> caseCount;
+	cout.precision(5);
+	for(int caseNumber = 1; caseNumber <= caseCount; ++caseNumber){
+		dataFile >> velocity >> distance;
+		theta = asin(distance * G / (2 * velocity * velocity)) * 180.0 / PI;
+		resultFile << "Case #" << caseNumber << ": " << ios::fixed << setprecision(6) << theta << endl;
+	}
 }
