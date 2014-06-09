555ca448ec4fea9c809bb0b659a12b62d6de3fac
teddy-hoo <hulingchuan@hotmail.com>
2014-06-09 18:11:41 +0800

    finish program arch

--- rationalNumberTree/rationalNumberTree.cpp
+++ rationalNumberTree/rationalNumberTree.cpp
@@ -20,11 +20,13 @@ int main(){
 	dataFile >> caseCount;
 	for(caseNumber = 1; caseNumber <= caseCount; ++caseNumber){
 		dataFile >> problemType;
+		cout << problemType << endl;
 		if(problemType == 1){
 			findElement.clear();
 			p = 1;
 			q = 1;
 			dataFile >> n;
+			cout << n << endl;
 			while(n != 1){
 				findElement.push_back(n % 2 ? true : false);
 				n = n / 2;
@@ -33,7 +35,19 @@ int main(){
 				p = findElement[i] ? p + q : p;
 				q = findElement[i] ? p - q : p + q;
 			}
-			
+			resultFile << "Case #" << caseNumber << ": " << p << " " << q << endl;
+		}
+		else{
+			n = 1;
+			dataFile >> p >> q;
+			cout << p << " " << q << endl;
+			while(p > 1 || q > 1){
+				cout << p << " " << q << endl;
+				n = p > q ? n * 2 + 1 : n * 2;
+				p = p > q ? p - q : p;
+				q = p > q ? q : q - p;
+			}
+			resultFile << "Case #" << caseNumber << ": " << n << endl;
 		}
 	}
 	return 0;
